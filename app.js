ac413445c0b8d372d736e7f11ba371a73296c4d1
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-03-16 22:59:04 +0900

    1-6 페이지 router 구현

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   //Get은 method / 그다음 주소는 가져올 url / 마지막은 비동기를 사용할지 boolean으로 변환
@@ -15,27 +18,37 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
   newsList.push("<ul>");
-  for (let i = 0; i < newsFeed.length; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-          <a href="#${newsFeed[i].id}">
+          <a href="#/show/${newsFeed[i].id}">
               ${newsFeed[i].title}(${newsFeed[i].comments_count})
           </a>
       </li>
       `);
   }
   newsList.push("</ul>");
+  newsList.push(`
+    <div>
+        <a href="#/page/${
+          store.currentPage > 1 ? store.currentPage - 1 : 1
+        }">이전 페이지</a>
+        <a href="#/page/${
+          store.currentPage < 3 ? store.currentPage + 1 : 3
+        }">다음 페이지</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
   container.innerHTML = `
       <h1>${newsContent.title}</h1>
       <div>
-          <a href="#">목록으로</a>
+          <a href="#/page/${store.currentPage}">목록으로</a>
       </div>
     `;
 }
@@ -43,6 +56,9 @@ function newsDetail() {
 function router() {
   const routePath = location.hash; //location.hash에 #만 있을경우는 비어있다고 봄
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,6 +123,9 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+var store = {
+  currentPage: 1
+};
 
 function getData(url) {
   //Get은 method / 그다음 주소는 가져올 url / 마지막은 비동기를 사용할지 boolean으로 변환
@@ -136,24 +139,28 @@ function newsFeed() {
   var newsList = [];
   newsList.push("<ul>");
 
-  for (var i = 0; i < newsFeed.length; i++) {
-    newsList.push("\n      <li>\n          <a href=\"#".concat(newsFeed[i].id, "\">\n              ").concat(newsFeed[i].title, "(").concat(newsFeed[i].comments_count, ")\n          </a>\n      </li>\n      "));
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n      <li>\n          <a href=\"#/show/".concat(newsFeed[i].id, "\">\n              ").concat(newsFeed[i].title, "(").concat(newsFeed[i].comments_count, ")\n          </a>\n      </li>\n      "));
   }
 
   newsList.push("</ul>");
+  newsList.push("\n    <div>\n        <a href=\"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n        <a href=\"#/page/").concat(store.currentPage < 3 ? store.currentPage + 1 : 3, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n    </div>\n  "));
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  var id = location.hash.substring(1);
+  var id = location.hash.substring(7);
   var newsContent = getData(CONTENT_URL.replace("@id", id));
-  container.innerHTML = "\n      <h1>".concat(newsContent.title, "</h1>\n      <div>\n          <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n      </div>\n    ");
+  container.innerHTML = "\n      <h1>".concat(newsContent.title, "</h1>\n      <div>\n          <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n      </div>\n    ");
 }
 
 function router() {
   var routePath = location.hash; //location.hash에 #만 있을경우는 비어있다고 봄
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
